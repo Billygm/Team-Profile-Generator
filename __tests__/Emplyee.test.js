96edd22364a1816fa5bfe70bc899b84da9be3029
Billy Mott <billygm@uw.edu>
2022-01-23 13:54:38 -0800

    adding some starting functions to my files

---
+++ __tests__/Emplyee.test.js
@@ -0,0 +1,28 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+
+    describe("Initialization / Constructor", () => {
+        
+        it("should build with prop x when give y", () => {
+            
+        })
+
+    });
+    describe("getName", () => {
+        
+    });
+
+    describe("getId", () => {
+
+    });
+
+    describe("getEmail", () => {
+
+    });
+
+    describe("getRole", () => {
+
+    });
+
+});
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require("./lib/Manager")
 
 const employees = [];
+
+
+employees.push(new Manager())
