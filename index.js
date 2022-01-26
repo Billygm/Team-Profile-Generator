8e799a4981247e64f01f39b84b625e3804c3e2f2
Billy Mott <billygm@uw.edu>
2022-01-26 12:57:00 -0800

    put my questions in variables to organize code better

--- index.js
+++ index.js
@@ -6,124 +6,135 @@ const Intern = require("./lib/Intern");
 
 const employees = [];
 
+const managerQuestions = [
+    {
+        type: "input",
+        message: "Input the Managers name",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Input the Managers ID number",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "Input the Managers email",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Input the Managers officeNumber",
+        name: "officeNumber",
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: "input",
+        message: "Input the Engineers name",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Input the Engineers ID number",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "Input the Engineers email",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Input the Engineers GitHub username",
+        name: "github",
+    }
+];
+
+const internQuestions = [
+    {
+        type: "input",
+        message: "Input the Interns name",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Input the Interns ID number",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "Input the Interns email",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Input the Interns school",
+        name: "school",
+    }
+];
+
+const nextActionQuestion = {
+        type: "input",
+        message: "what would you like to do next?",
+        choices: [
+            "Add an Engineer",
+            "Add an Intern",
+            "Done adding employees",
+        ]
+};
+
+
+async function askForManagerInfo() {
+    const answers = await inquirer.prompt(managerQuestions)
+    
+    employees.push(new Manager(answers));
+
+    console.log(`Manager ${managerName} successfully added.`)
+    
+    askForNextAction();
+};
+
+async function askForEngineerInfo() {
+    const answers = await inquirer.prompt(engineerQuestions)
+    
+    employees.push(new Engineer(answers));
+
+    console.log(`Engineer ${engineerName} successfully added.`)
+    
+    askForNextAction();
+};
+
+async function askForInternInfo() {
+    const answers = await inquirer.prompt(internQuestions)
+    
+    employees.push(new Intern(answers));
+
+    console.log(`Intern ${internName} successfully added.`)
+    
+    askForNextAction();
+};
+
+async function askForNextAction() {
+    const answer = await inquirer.prompt(nextActionQuestion)
+    console.log(answer)
+
+
+    if(answer.nextquestion === "Add an Engineer"){
+        askForEngineerInfo()
+    } else if(answer.nextquestion === "Add an Intern"){
+        askForInternInfo()
+    } else if(answer.nextquestion === "Done adding employees"){
+        fs.writeFile("./dist/team.html", generateHtml(employees), (err) =>
+        err ? console.error(err) : console.log("Success! Team Created!")
+    );
+
+};
+
+function generateHtml(employees) {
+    return employees
+}
+
+
 askForManagerInfo()
-
-function askForManagerInfo() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "Input the Managers name",
-                name: "title",
-            },
-            {
-                type: "input",
-                message: "Input the Managers ID number",
-                name: "id",
-            },
-            {
-                type: "input",
-                message: "Input the Managers email",
-                name: "email",
-            },
-            {
-                type: "input",
-                message: "Input the Managers officeNumber",
-                name: "officeNumber",
-            }
-        ])
-        .then((answers) => {
-
-            employees.push(new Manager(answers));
-
-            askForNextAction();
-        })
-}
-
-function askForNextAction() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "what would you like to do next?",
-                choices: [
-                    "Add an Engineer",
-                    "Add an Intern",
-                    "Done adding employees",
-                ]
-            }
-        ])
-        .then((answer) => {
-            if(answer === "Add an Engineer"){
-                askForEngineerInfo()
-            } else if(answer === "Add an Intern"){
-                askForInternInfo()
-            } else if(answer === "Done adding employees"){
-                generateHtml()
-            }
-        })
-}
-
-function askForEngineerInfo() {
-    inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Input the Engineers name",
-            name: "title",
-        },
-        {
-            type: "input",
-            message: "Input the Engineers ID number",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Input the Engineers email",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Input the Engineers GitHub username",
-            name: "github",
-        }
-    ])
-    .then((answers) => {
-
-        employees.push(new Engineer(answers));
-
-        askForNextAction();
-    })
-}
-
-function askForInternInfo() {
-    inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Input the Interns name",
-            name: "title",
-        },
-        {
-            type: "input",
-            message: "Input the Interns ID number",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Input the Interns email",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Input the Interns school",
-            name: "school",
-        }
-    ])
-    .then((answers) => {
-
-        employees.push(new Intern(answers));
-
-        askForNextAction();
-    })
-}
